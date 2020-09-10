cbf1998af9913d426a9dd51ec78f12c4d1b389ec
Anthony Allgood <62567954+Allyogi84@users.noreply.github.com>
2020-09-10 09:30:06 -0400

    completed code

---
+++ ThreeWheeler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace FactoryPatternExercise
+{
+    public class ThreeWheeler : IVehicle
+    {
+        public ThreeWheeler()
+        {
+        }
+        public void Drive()
+        {
+            Console.WriteLine("Let's get going playa!");
+
+        }
+
+
+    }
+}
